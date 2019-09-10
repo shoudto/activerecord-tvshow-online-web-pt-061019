d898226879194dca64a003434e10503a56778484
Oscar Shoudt <shoudto@gmail.com>
2019-09-10 21:11:10 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -12,19 +12,19 @@ class Show < ActiveRecord::Base
     self.minimum(:rating)
   end
 
-  def least_popular_show
+  def self.least_popular_show
     self.where("rating = ?", self.lowest_rating).first
   end
 
-  def ratings_sum
+  def self.ratings_sum
     self.sum(:rating)
   end
 
-  def popular_shows
+  def self.popular_shows
     self.where("rating > ?", 5)
   end
 
-  def shows_by_alphabetical_order
+  def self.shows_by_alphabetical_order
     self.order(name: :asc)
   end
 end
