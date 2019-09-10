ee5f53ff5bd23c54145c19a7e35a8d59c1215c20
Oscar Shoudt <shoudto@gmail.com>
2019-09-10 20:43:58 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -9,9 +9,11 @@ class Shows < ActiveRecord::Base
   end
 
   def self.lowest_rating
+    self.minimum(:rating)
   end
 
   def least_popular_show
+    self.where("rating = ?", self.lowest_rating).first
   end
 
   def ratings_sum
