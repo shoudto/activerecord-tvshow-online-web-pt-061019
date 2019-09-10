f9ecfd569e4ccf411b9ce851d0a3ef55b29e900d
Oscar Shoudt <shoudto@gmail.com>
2019-09-10 20:41:55 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,2 +1,24 @@
 class Shows < ActiveRecord::Base
+
+  def self.highest_rating 
+    self.maximum(:rating)
+  end 
+
+  def self.most_popular_show
+  end  
+
+  def self.lowest_rating
+  end 
+
+  def least_popular_show
+  end 
+
+  def ratings_sum
+  end 
+
+  def popular_shows
+  end 
+
+  def shows_by_alphabetical_order
+  end 
 end
