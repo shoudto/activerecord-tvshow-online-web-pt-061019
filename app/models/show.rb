f8cea3060c36e2354658d306e6f4c4c2ef486071
Oscar Shoudt <shoudto@gmail.com>
2019-09-10 20:54:51 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,30 +1,30 @@
-class Shows < ActiveRecord::Base
+class Show < ActiveRecord::Base
 
-  def self.highest_rating
-    self.maximum(:rating)
-  end
-
-  def self.most_popular_show
-    self.where("rating = ?", self.highest_rating).first
-  end
-
-  def self.lowest_rating
-    self.minimum(:rating)
-  end
-
-  def least_popular_show
-    self.where("rating = ?", self.lowest_rating).first
-  end
-
-  def ratings_sum
-    self.sum(:rating)
-  end
-
-  def popular_shows
-    self.where("rating > ?", 5)
-  end
-
-  def shows_by_alphabetical_order
-    self.order(name: :asc)
-  end
+  # def self.highest_rating
+  #   self.maximum(:rating)
+  # end
+  # 
+  # def self.most_popular_show
+  #   self.where("rating = ?", self.highest_rating).first
+  # end
+  # 
+  # def self.lowest_rating
+  #   self.minimum(:rating)
+  # end
+  # 
+  # def least_popular_show
+  #   self.where("rating = ?", self.lowest_rating).first
+  # end
+  # 
+  # def ratings_sum
+  #   self.sum(:rating)
+  # end
+  # 
+  # def popular_shows
+  #   self.where("rating > ?", 5)
+  # end
+  # 
+  # def shows_by_alphabetical_order
+  #   self.order(name: :asc)
+  # end
 end
